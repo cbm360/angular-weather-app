4a2d14b44361373a51757b71b19e14953f079c3f
CAMRON MOORE <camronmoore@CAMRONs-MBP.lan>
2016-06-11 14:21:04 -0600

    Added links to change the number of days in the forcast and bound to new route

--- app.js
+++ app.js
@@ -41,13 +41,15 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
     $scope.city = cityService.city;
 
+    $scope.days = $routeParams.days || 2;
+
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?appid=6a70be23b6e0f8534a927ec417e02021&units=imperial");
 
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
 
     $scope.convertToDate = function(dt) {
         return new Date(dt * 1000);
